e5db1f54f155d73184ef3aef04dd9446b6c9cce0
DeepakMehra53 <dmehra594@gmail.com>
2024-12-14 20:26:27 +0530

    LineUnderNav

---
+++ Portfolio/src/components/LineUnderNav/LineUnderNav.jsx
@@ -0,0 +1,12 @@
+export default function LineUnderNav(){
+    return (
+      <div className="add_wr">
+        <div className="line_wrap nav">
+          <div
+            data-w-id="045f99fe-ac6b-eb28-1f16-d72c0e5c608d"
+            className="h-[2px] line_nav"
+          ></div>
+        </div>
+      </div>
+    );
+}
