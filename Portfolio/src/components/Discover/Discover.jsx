d5d3c6aa581e340a82c1cc322be5f05ce02e53ef
DeepakMehra53 <dmehra594@gmail.com>
2024-12-24 19:01:09 +0530

    Introduction line adjust

--- Portfolio/src/components/Discover/Discover.jsx
+++ Portfolio/src/components/Discover/Discover.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Discover = () => {
      const textContent = [
-       "My name is Deepak Mehra, and I am a recent graduate with a BTech in Computer Science",
-       "specializing in full-stack development and passionate about building innovative software solutions.",
-       "With expertise in front-end and back-end technologies, I create responsive, efficient, and scalable applications.",
+       "My name is Deepak Mehra,and I am a recent",
+       "graduate with a BTech in Computer Science",
+       "specializing in full-stack development and,",
+       " passionate about building innovative software ",
+       "solution. With expertise in front-end and back-end",
+       "technologies,I create responsive, efficient, and scalable applications.",
+       "My skill set includes React, Node.js, AWS, Python, and more.",
      ];
 
      const mobileTextContent = [
