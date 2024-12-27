c7f9c6904dc4008f126be702540ee67bafa8759e
DeepakMehra53 <dmehra594@gmail.com>
2024-12-28 00:39:31 +0530

    Now god please

--- Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
+++ Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
@@ -1,10 +1,13 @@
-
+import Rightarrow from "../../assets/Rightarrow.svg"
 import { ProjectIntroImages } from "../ProjectIntroImages/ProjectIntroImages"
 export const ProjectIntro = () => {
   return (
     <div className="Konsul_2">
         <div className="_43 sqaure">
             <ProjectIntroImages/>
+            <div className="arrow_2_wr">
+                <img src={Rightarrow} alt="" loading="lazy"  className="aroow_2"/>
+            </div>
         </div>
     </div>
   )
--- Portfolio/src/components/ProjectIntroImages/ProjectIntroImages.jsx
+++ Portfolio/src/components/ProjectIntroImages/ProjectIntroImages.jsx
@@ -1,15 +1,17 @@
 import First from "../../assets/First.svg"
 import Second from "../../assets/Second.svg"
 import Third from "../../assets/Third.svg"
+import Forth from "../../assets/Forth.svg"
+import Fifth from "../../assets/Fifth.svg"
 
 export const ProjectIntroImages = () => {
   return (
     <div className="sqare_wrap">
-        <img src={First} alt="" />
-        <img src={Second} alt="" />
-        <img src={Third} alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={First} alt="" className="sqare_4"/>
+        <img src={Second} alt="" className="sqare_3"/>
+        <img src={Third} alt="" className="sqare_2"/>
+        <img src={Forth} alt="" className="sqare_1"/>
+        <img src={Fifth} alt="" className="sqare_1_2"/>
     </div>
   )
 }
