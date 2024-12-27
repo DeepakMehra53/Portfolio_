0e05bfee8497c660f20aed135ab9308a82b4a4da
DeepakMehra53 <dmehra594@gmail.com>
2024-12-27 21:47:47 +0530

    Projest style squar figma added

--- Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
+++ Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
@@ -1,11 +1,22 @@
-
+import {Firstsquar} from "../../assets/Firstsquar.svg";
+import { Secondsquar } from "../../assets/Secondsquar.svg";
+import { Thirdsquar } from "../../assets/Thirdsquar.svg";
+import { Forthsquar } from "../../assets/Forthsquar.svg";
+import {Fifthsquar} from "../../assets/Fifthsquar.svg";
 
 export const ProjectIntro = () => {
   return (
     <div className="konsul_2">
-        <div>
-            
+      <div className="_43 square">
+        <div data-w-id="acd26c56-3acd-1b27-078c-ece08e09e9a3"className="square_wrap">
+            <img src={Firstsquar} alt="" />
+            <img src={Secondsquar} alt="" />
+            <img src={Thirdsquar} alt="" />
+            <img src={Forthsquar} alt="" />
+            <img src={Fifthsquar} alt="" />
         </div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
