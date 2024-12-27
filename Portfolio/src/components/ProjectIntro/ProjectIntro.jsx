9e46608aa00524083af652b938f161099d3118cc
DeepakMehra53 <dmehra594@gmail.com>
2024-12-27 21:57:40 +0530

    opacity:0

--- Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
+++ Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
@@ -9,11 +9,11 @@ export const ProjectIntro = () => {
     <div className="konsul_2">
       <div className="_43 square">
         <div data-w-id="acd26c56-3acd-1b27-078c-ece08e09e9a3"className="square_wrap">
-            <img src={Firstsquar} alt="" className="sqare_4"/>
+            <img data-w-id="acd26c56-3acd-1b27-078c-ece08e09e9a3" style="opacity:0" src={Firstsquar} alt="" className="sqare_4"/>
             <img src={Secondsquar} alt="" className="sqare_3"/>
             <img src={Thirdsquar} alt="" className="sqare_2"/>
             <img src={Forthsquar} alt="" className="sqare_1"/>
-            <img src={Fifthsquar} alt="" className="sqare_1_2" />
+            <img data-w-id="9f25df8a-3d1c-dc43-3114-b4dcf18c248c" src={Fifthsquar} alt="" className="sqare_1_2" />
         </div>
         <div></div>
       </div>
