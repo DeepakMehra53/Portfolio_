2b46e565eaec50646bb7ddc61fd124790bb33ded
DeepakMehra53 <dmehra594@gmail.com>
2024-12-28 10:36:13 +0530

    import introduction

--- Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
+++ Portfolio/src/components/ProjectIntro/ProjectIntro.jsx
@@ -1,6 +1,6 @@
 import Rightarrow from "../../assets/Rightarrow.svg"
 import { ProjectIntroImages } from "../ProjectIntroImages/ProjectIntroImages"
-
+import { Introductiontoproject } from "../Introductiontoproject/ProjectDescription"
 export const ProjectIntro = () => {
   return (
     <div className="Konsul_2">
@@ -9,7 +9,7 @@ export const ProjectIntro = () => {
             <div className="arrow_2_wr">
                 <img src={Rightarrow} alt="" loading="lazy"  className="aroow_2"/>
             </div>
-            
+            <Introductiontoproject/>
         </div>
     </div>
   )
